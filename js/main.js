75f8a3b59573004e14cad5970a4c723b85e94859
Katie Kowalsky <kkowalsky@wisc.edu>
2014-11-22 08:30:21 -0600

    playing with more mmenu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 <script type="text/javascript">
    $(document).ready(function() {
       $("#map-menu").mmenu({
-          
+          //don't need to add click events? wow much magic
+          //this might be the hardest for me to code. but maybe not? Cider's on me if I'm wrong
+          header: true
+          classes: "mm-light"
       });
    });
 </script>
