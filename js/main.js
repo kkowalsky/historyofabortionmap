9b99cc44b491fbe1ae5308ca35dc72257e28228c
Katie Kowalsky <kkowalsky@wisc.edu>
2014-11-29 19:34:23 -0600

    add us.json, start building map svg

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-<script type="text/javascript">
+//global variables
+var mapWidth = 800, mapHeight = 600;
+
+window.onload = initialize();
+
+function initialize(){
    $(document).ready(function() {
       $("#map-menu").mmenu({
           //don't need to add click events? wow much magic
@@ -7,5 +12,31 @@
           classes: "mm-light"
       });
    });
-   // HELLO MY NAME IS ROBIN
-</script>
+    
+    setMap();
+}
+   //creates map
+function setMap(){
+    var map = d3.select("body")
+        .append("svg")
+        .attr("width", mapWidth)
+        .attr("height", mapHeight)
+        .attr("class", "map");
+    
+    //Create a Albers equal area conic projection
+    var projection = d3.geo.albersUsa();
+    
+    //create svg path generator using the projection
+    var path = d3.geo.path()
+        .projection(projection);
+    
+    queue()
+        .defer(d3.json, "data/us.json")
+        .await(callback);
+    
+    
+    //retrieve and process json file and data
+    function callback(error, us){
+        
+    };
+}
