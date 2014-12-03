d9b38887a1e2a33c5ddbbf22fad76c4bd1960f71
tolomaps <robin.tolochko@gmail.com>
2014-12-02 21:40:56 -0600

    Drew the map
    Added new topojson file, deleted old json files, drew the map svg

--- js/main.js
+++ js/main.js
@@ -13,32 +13,33 @@ function initialize(){
 }; //END initialize
 //creates map
 function setMap(){
-    var map = d3.select("body")
+    var map = d3.select(".usmap")
         .append("svg")
         .attr("width", mapWidth)
         .attr("height", mapHeight)
-        .attr("class", "usmap");
+        .attr("class", "map");
     
     //Create a Albers equal area conic projection
-    var projection = d3.geo.albersUsa();
+    var projection = d3.geo.albersUsa()
+        .scale(1000)
+        .translate([mapWidth / 2, mapHeight / 2]);
     
     //create svg path generator using the projection
     var path = d3.geo.path()
         .projection(projection);
     
     queue()
-        .defer(d3.json, "data/usa.json")
+        .defer(d3.json, "data/usa.topojson")
         .await(callback);
     
     
     //retrieve and process json file and data
     function callback(error, usa){
-        console.log(usa);
        //TODO: draw map
-        
-         //add usa geometry
+        console.log(usa);
+        // add usa geometry
         var states = map.append("path") //create SVG path element
-            .datum(topojson.feature(usa, usa.objects.usa))
+            .datum(topojson.feature(usa, usa.objects.states))
             .attr("class", "states") //class name for styling
             .attr("d", path); //project data as geometry in svg
         
