5678821f3d05500bc345d05c7dadd874af5b5cce
DylanMoriarty <EatinCake@gmail.com>
2014-12-04 19:21:00 -0600

    Greyscale Rainbow Generator - Start

--- js/main.js
+++ js/main.js
@@ -1,41 +1,12 @@
 /******* GLOBAL VARIABLES *******/
 var mapWidth = 850, mapHeight = 600;
 var yearsArray = ["grade", "Pre-1973", "1973", "1974", "1975", "1976", "1977", "1977","1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014"];
+var Category = ["gradeData", "ConsentData"]
+var expressed = Category[0]
 var removeCPC;
 var removeAbortion;
 
 window.onload = initialize();
-
-//SET UP COLOR ARRAYS FOR MAP + CHART
-// Color array for Overview & Waiting Period
-    colorArrayOverview = [  "#252525",      //F     //72 hours
-                            "#636363",      //D     //48 hours
-                            "#969696",      //C     //24 hours
-                            "#cccccc",      //B     //18 hours
-                            "#f7f7f7"   ];  //A     //None
-
-    // Color array for Prohibited At
-    colorArrayProhibited = ["#252525",      //12 weeks
-                            "#636363",      //20 weeks
-                            "#969696",      //22 weeks
-                            "#bdbdbd",      //24 weeks
-                            "#d9d9d9",      //3rd trimester
-                            "#f7f7f7"   ];  //Viability
-
-    // Color array for Mandated Counseling
-    colorArrayCounseling = ["#252525",      //Yes
-                            "#f7f7f7"   ];  //No
-
-    // Color array for Parental Consent
-    colorArrayConsent = [   "#252525",      //Consent
-                            "#969696",      //Notice
-                            "#f7f7f7"   ];  //None
-
-    // Color array for Ultrasound
-    colorArrayUltrasound = ["#252525",      //Must be performed, offer to view
-                            "#636363",      //Must be performed
-                            "#969696",      //Must be offered
-                            "#f7f7f7"   ];  //None
 
 // SET UP ARRAYS FOR CATEGORIES OF EACH VARIABLE
 
@@ -121,6 +92,9 @@ function setMap(){
     
     //retrieve and process json file and data
     function callback(error, consent, grade, usa, cpc, abortionprovider){
+
+        colorize = colorScale(grade);
+
         var states = map.append("path") //create SVG path element
             .datum(topojson.feature(usa, usa.objects.states))
             .attr("class", "states") //class name for styling
@@ -160,27 +134,29 @@ function setMap(){
                         };
 
                     jsonStates[a].properties[attribute] = attrObj;
-                    console.log(jsonStates[a].properties)
+//                    console.log(jsonStates[a].properties)
 
                     break;
                     };
                 };
              }; 
         }; //END linkData
+//        console.log((topojson.feature(usa, usa.objects.states).features))
 
-        var choropleth = map.selectAll(".states")
+        var choroplethoverlay = map.selectAll(".states")
             .data(topojson.feature(usa, usa.objects.states).features)
             .enter()
             .append("g")
             .attr("class", "choropleth")
             .append("path")
-            .attr("class", function(d){ return d.properties.postal})
-            .attr("d", path);
-/*            .append("desc")
+            .attr("class", function(d){ 
+                return d.properties.postal})
+            .attr("d", path)
+            .append("desc")
                 .text(function(d){
                     return choropleth(d, colorize);
                 })
-*/
+
 // -- Grab State Abv. from TopoJSON -- (usa.objects.states.geometries[1].properties.postal)
         //data stuff for overlay
         var cpcCount = [];
@@ -274,26 +250,52 @@ function changeAttribute(attribute, data) {
 
 };
 
-//color generator for country choropleth
+//---------------------------------------------//
+/* BEAUTIFUL GREYSCALE RAINBOW COLOR GENERATOR */
+//---------------------------------------------//
+//         colorize = colorScale(consent, grade);
+//SET UP COLOR ARRAYS FOR MAP + CHART
+// Color array for Overview & Waiting Period   
 function colorScale(csvData){
-    var color = d3.scale.ordinal() //use ordinal scale since the variables are all specific value
-        .range([
-            "#f7f7f7",
-            "#cccccc",
-            "#969696",
-            "#636363",
-            "#252525"
-            ]);
-//!! Below area temporarly commented out until we write the [expressed] value !!//
+    colorArrayOverview = [  "#252525",      //F     //72 hours
+                            "#636363",      //D     //48 hours
+                            "#969696",      //C     //24 hours
+                            "#cccccc",      //B     //18 hours
+                            "#f7f7f7"   ];  //A     //None
 
-//  var domainArray = [];
-//  for (var i in csvData){
-//    if(csvData[i][expressed] != 0){
-//    domainArray.push(Number(csvData[i][expressed]));
-//    };
-//  };
-//  color.domain(domainArray);
-//  return color; 
-}; //END colorScale
+    //Color array for Prohibited At
+    colorArrayProhibited = ["#252525",      //12 weeks
+                            "#636363",      //20 weeks
+                            "#969696",      //22 weeks
+                            "#bdbdbd",      //24 weeks
+                            "#d9d9d9",      //3rd trimester
+                            "#f7f7f7"   ];  //Viability
+
+    // Color array for Mandated Counseling
+    colorArrayCounseling = ["#252525",      //Yes
+                            "#f7f7f7"   ];  //No
+
+    // Color array for Parental Consent
+    colorArrayConsent = [   "#252525",      //Consent
+                            "#969696",      //Notice
+                            "#f7f7f7"   ];  //None
+
+    // Color array for Ultrasound
+    colorArrayUltrasound = ["#252525",      //Must be performed, offer to view
+                            "#636363",      //Must be performed
+                            "#969696",      //Must be offered
+                            "#f7f7f7"   ];  //None 
+};
+
+function choropleth(d, colorize){
+    var value = d.properties ? d.properties[expressed] : d[expressed];
+
+    if (value) {
+        return colorize(value);
+    }else{
+        return "#ccc"
+    }
+};
+
 
 //TODO: animated sequence buttons
